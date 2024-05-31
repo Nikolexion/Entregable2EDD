d37a316b158cf0a4550de2a1c783bd7e016ed5c2
Nikolexion <nfalc2003@gmail.com>
2024-05-31 18:19:53 -0400

    feat: creado el struct User

---
+++ User.cpp
@@ -0,0 +1,12 @@
+#include<string>
+
+struct User
+{
+    std::string university;
+    int userId;
+    std::string username;
+    int numberTweets;
+    int friendsCount;
+    int followersCount;
+    std::string createdAt;
+};
