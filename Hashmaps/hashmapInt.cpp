e1b0afcc0246dcc56e0f30168846cddec0b662ab
Nikolexion <nfalc2003@gmail.com>
2024-06-04 01:39:40 -0400

    feat: documentados hashmaps abiertos

--- Hashmaps/hashmapInt.cpp
+++ Hashmaps/hashmapInt.cpp
@@ -7,9 +7,11 @@
 
 class HashAbiertoInt {
 private:
+    //Creamos un vector de listas de pares de enteros(keys) y usuarios
     std::vector<std::list<std::pair<int,User>>> table;
     size_t table_size;
 
+    //Función hash que retorna el módulo de la key con el tamaño de la tabla
     size_t hash(int key) const {
         return key % table_size;
     }
@@ -19,32 +21,35 @@ public:
         table.resize(size);
     }
 
+    //Insertamos un usuario en la tabla hash
     void insert(int key, User usuario) {
-        size_t index = hash(key);
-        for (auto& pair : table[index]) {
-            if (pair.first == key) {
-                pair.second = usuario;
+        size_t index = hash(key); //Obtenemos el índice de la tabla
+        for (auto& pair : table[index]) { //Recorremos la lista en la posición index
+            if (pair.first == key) { //Si la key ya existe, actualizamos el usuario
+                pair.second = usuario; 
                 return;
             }
         }
-        table[index].emplace_back(key, usuario);
+        table[index].emplace_back(key, usuario); //Si no existe, insertamos el par key-usuario
     }
 
+    //Buscamos un usuario en la tabla hash
     bool search(int key, User& usuario) const {
-        size_t index = hash(key);
-        for (const auto& pair : table[index]) {
+        size_t index = hash(key); //Obtenemos el índice de la tabla
+        for (const auto& pair : table[index]) { //Recorremos la lista en la posición index
             if (pair.first == key) {
                 usuario = pair.second;
                 return true;
             }
         }
-        return false;
+        return false; //Si no encontramos la key, retornamos false
     }
 
+    //Eliminamos un usuario de la tabla hash
     bool remove(int key) {
-        size_t index = hash(key);
-        for (auto it = table[index].begin(); it != table[index].end(); ++it) {
-            if (it->first == key) {
+        size_t index = hash(key); //Obtenemos el índice de la tabla
+        for (auto it = table[index].begin(); it != table[index].end(); ++it) { //Recorremos la lista en la posición index
+            if (it->first == key) { //Si encontramos la key, la eliminamos
                 table[index].erase(it);
                 return true;
             }
--- Hashmaps/hashmapString.cpp
+++ Hashmaps/hashmapString.cpp
@@ -7,9 +7,11 @@
 
 class HashAbiertoString {
 private:
+    //Creamos un vector de listas de pares de enteros(keys) y usuarios
     std::vector<std::list<std::pair<std::string, User>>> table;
     size_t table_size;
 
+    //Función hash que retorna el valor de la key multiplicado por 33 y sumado con el valor ASCII de cada caracter de la key
     size_t hash(const std::string& key) const {
         size_t hash_value = 0;
         for (char ch : key) {
@@ -23,32 +25,35 @@ public:
         table.resize(size);
     }
 
+    //Insertamos un usuario en la tabla hash
     void insert(std::string key, User usuario) {
-        size_t index = hash(key);
-        for (auto& pair : table[index]) {
-            if (pair.first == key) {
+        size_t index = hash(key); //Obtenemos el índice de la tabla
+        for (auto& pair : table[index]) { //Recorremos la lista en la posición index
+            if (pair.first == key) { //Si la key ya existe, actualizamos el usuario
                 pair.second = usuario;
                 return;
             }
         }
-        table[index].emplace_back(key, usuario);
+        table[index].emplace_back(key, usuario); //Si no existe, insertamos el par key-usuario
     }
 
+    //Buscamos un usuario en la tabla hash
     bool search(std::string key, User& usuario) const {
-        size_t index = hash(key);
-        for (const auto& pair : table[index]) {
+        size_t index = hash(key); //Obtenemos el índice de la tabla
+        for (const auto& pair : table[index]) { //Recorremos la lista en la posición index
             if (pair.first == key) {
                 usuario = pair.second;
-                return true;
+                return true; //Si no encontramos la key, retornamos false
             }
         }
         return false;
     }
 
+    //Eliminamos un usuario de la tabla hash
     bool remove(std::string key) {
-        size_t index = hash(key);
-        for (auto it = table[index].begin(); it != table[index].end(); ++it) {
-            if (it->first == key) {
+        size_t index = hash(key); //Obtenemos el índice de la tabla
+        for (auto it = table[index].begin(); it != table[index].end(); ++it) { //Recorremos la lista en la posición index
+            if (it->first == key) { //Si encontramos la key, la eliminamos
                 table[index].erase(it);
                 return true;
             }
