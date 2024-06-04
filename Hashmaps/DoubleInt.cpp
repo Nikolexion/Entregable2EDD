2ceb2199cdd947a193dc3459a47d395e9e8e3c2b
Nikolexion <nfalc2003@gmail.com>
2024-06-04 11:22:14 -0400

    feat: documentado todo el DoubleInt

--- Hashmaps/DoubleInt.cpp
+++ Hashmaps/DoubleInt.cpp
@@ -5,15 +5,32 @@
 #include <chrono>
 #include "User.cpp"
 
+/**
+ * @class HashDoubleInt
+ * @brief Una implementacion de una tabla hash, usando double hashing con una key entera.
+ * 
+ * Esta clase representa una tabla hash que guarda pares, donde el primero es la key y el segundo es un objeto User.
+ * Ocupa double hashing como la estrategia de resolucion de colisiones.
+ */
 class HashDoubleInt {
 private:
-    std::vector<std::pair<int,User>> table;
-    size_t table_size;
+    std::vector<std::pair<int,User>> table; /**< El vector que almacena los pares key-User. */
+    size_t table_size; /**< El tamaño de la tabla hash. >*/
 
+    /**
+     * @brief Funcion hash que retorna el modulo de la key con el tamaño de la tabla.
+     * @param key La key a la que se le hará hash.
+     * @return EL valor hash.
+     */
     size_t hash(int key) const {
         return key % table_size;
     }
 
+    /**
+     * @brief Verifica si es un numero primo.
+     * @param n EL numero que será verificado.
+     * @return True si el número es primo. False en otro caso
+     */
     static bool isPrime(int n) {
         if (n <= 1) return false;
         if (n <= 3) return true;
@@ -27,6 +44,11 @@ private:
         return true;
     }
 
+    /**
+     * @brief Busca el mayor numero primo menor que el numero dado.
+     * @param n El numero para encontrar el mayor numero primo.
+     * @return El mayor numero primo menor que n.
+     */
     static int largestPrimeLessThan(int n) {
         for (int i = n - 1; i > 1; i--) {
             if (isPrime(i)) return i;
@@ -34,26 +56,45 @@ private:
         return 2;  
     }
 
+    /**
+     * @brief Segunda funcion hash que usa el numero primo mas cercano a el tamaño de la tabla.
+     * @param key La key a la que se le hará hash.
+     * @return EL valor hash.
+     */
     size_t hash2(int key) const {
         int prime = largestPrimeLessThan(table_size);
         return prime - (key % prime);
     }
 
 public:
+    /**
+     * @brief Construye un objeto de HashDoubleInt con el tamaño especificado.
+     * @param size EL tamaño de la tabla hash.
+     */
     HashDoubleInt(size_t size) : table_size(size) {
         table.resize(size);
     }
 
+    /**
+     * @brief Inserta un par key-User a la tabla hash.
+     * @param key La key a ser insertada.
+     * @param usuario EL User a ser insertado.
+     */
     void insert(int key, User usuario) {
         size_t index = hash(key);
         int count = 0;
-        while (table[index].first != 0) {
-            index = hash(hash(index) + count++*hash2(index));
+        while (table[index].first != 0 && table[index].first != -1) {
+            index = (hash(index) + count++*hash2(index)) % table_size; 
         }
-
         table[index] = {key, usuario};
     }
-
+    
+    /**
+     * @brief Busca por la key en la tabla hash y sobreescribe el Usuario entregado en el argumento con el usuario encontrad en la tabla hash.
+     * @param key La key que será buscada.
+     * @param usuario EL objeto User para guardar el resultado.
+     * @return True si la key es encontrada, False en otro caso.
+     */
     bool search(int key, User& usuario) const {
         size_t index = hash(key);
         int count = 1;
@@ -67,6 +108,11 @@ public:
         return false;
     }
 
+    /**
+     * @brief Elimina un par key-User de la tabla hash.
+     * @param key La kehy que será removida.
+     * @return True si la key es encontrada y eliminada, False en otro caso.
+     */
     bool remove(int key) {
         size_t index = hash(key);
         int count = 1;
@@ -92,7 +138,7 @@ int main(int argc, char** argv) {
         
     // Creamos n usuarios y los insertamos en la tabla hash
     auto start = std::chrono::high_resolution_clock::now();
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         User user;
         user.userId = i;
         hashTable.insert(i, user);
@@ -102,16 +148,17 @@ int main(int argc, char** argv) {
 
     // Buscamos cada usuario en la tabla hash
     start = std::chrono::high_resolution_clock::now();
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         User user;
-        hashTable.search(i, user);
+        //hashTable.search(i, user);
+        std::cout << "Buscando indice " << i << (hashTable.search(i, user) ? "Found" : "Not Found") <<"\n";
     }
     end = std::chrono::high_resolution_clock::now();
     double search_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() * 1e-9;
 
     // Eliminamos cada usuario de la tabla hash
     start = std::chrono::high_resolution_clock::now();
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         hashTable.remove(i);
     }
     end = std::chrono::high_resolution_clock::now();
